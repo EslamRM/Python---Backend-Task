00bab770c6ea2fcad9a10478cf59883e72a347c0
Eslam Ramadan <55287149+EslamRM@users.noreply.github.com>
2023-01-19 23:35:37 +0400

    Update settings.py

--- BackendTask/settings.py
+++ BackendTask/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'taskdatabase',  
         'USER': 'root',  
         'PASSWORD': '',  
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
